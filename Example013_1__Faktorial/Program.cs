d90a37297fa7b302d97690a5740f30d3395077c0
maxim <m.krishnevskiy@kmg.kz>
2022-07-10 15:21:28 +0600

    Lekciya 4

---
+++ Example013_1__Faktorial/Program.cs
@@ -0,0 +1,16 @@
+﻿//int Factorial(int n) //int не работает с большими числами
+double Factorial(int n)
+{
+    // 1! = 1 
+    // 0! = 1
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+
+}
+for (int i = 1; i < 40; i++)
+{
+    //Console.WriteLine(Factorial(i)); 
+    Console.WriteLine($"{i}! = {Factorial(i)}"); 
+}
+// Console.WriteLine(Factorial(3)); //1*2*3 = 6
+// Console.WriteLine(Factorial(5)); 
